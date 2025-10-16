851cb5f6cc4a6de37643ac8aefb4be08b4a5936b
Minhhao04 <minhhaodoan123@gmail.com>
2025-10-16 13:02:28 +0700

    new commit

--- backend/index.ts
+++ backend/index.ts
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://your-frontend.vercel.app"],
+    origin: ["http://localhost:3000", "https://assignmen1-hao.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
